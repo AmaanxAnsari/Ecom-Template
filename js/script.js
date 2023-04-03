37a24544832c02e036c47b0404113c27d4104c73
Amaan Salim Ansari <amaan0076@gmail.com>
2023-04-03 13:07:57 +0530

    Version 1.0

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $('.service-slider').slick({
+        arrows: false,
         infinite: true,
         autoplay: true,
         autoplaySpeed: 2000,
         speed: 3000,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: false,
         responsive: [
             {
                 breakpoint: 1024,
@@ -14,7 +14,6 @@ $(document).ready(function () {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                     infinite: true,
-                    dots: true
                 }
             },
             {
@@ -34,9 +33,7 @@ $(document).ready(function () {
         ]
     }),
         $('.brand-slider').slick({
-            arrows: true,
-            prevArrow: '<button type="button" class="slick-prev" style="color:#f15060"></button>',
-            nextArrow: '<button type="button" class="slick-next" style="color:#f15060"></button>',
+            arrows: false,
             infinite: true,
             speed: 3000,
             slidesToShow: 1,
@@ -48,7 +45,39 @@ $(document).ready(function () {
                         slidesToShow: 1,
                         slidesToScroll: 1,
                         infinite: true,
-                        dots: true
+
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        }),
+        $('.test-slider').slick({
+            arrows: false,
+            infinite: true,
+            speed: 3000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+
                     }
                 },
                 {
